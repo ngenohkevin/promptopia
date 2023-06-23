c48224a92303ffb938681bec371f2c0cb688772f
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-23 14:20:15 +0300

    add google auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,18 +7,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
 
-const isUserLoggedIn = true
+const { data: session } = useSession();
 
 const [providers, setProviders] = useState(null)
 const [toggleDropDown, setToggleDropDown] = useState(false)
 
 useEffect(() => {
-  const setProviders = async () => {
+  const setUpProviders = async () => {
     const response = await getProviders();
 
     setProviders(response);
   }
-  setProviders();
+  setUpProviders();
 }, [])
 
 
@@ -37,9 +37,12 @@ useEffect(() => {
           Promptopia
         </p>
       </Link>
-      {/* Mobile */}
+
+
+
+      {/* Desktop */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -49,7 +52,7 @@ useEffect(() => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -73,10 +76,10 @@ useEffect(() => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
