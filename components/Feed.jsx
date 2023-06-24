d427acd5b07f068b80ceeceea7af9add7bb4d21b
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-24 18:43:57 +0300

    new feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,9 +4,19 @@ import { useState, useEffect } from "react"
 import PromptCard from "./PromptCard";
 
 
-const PromptCardList ({data, handleTagClick}) => {
-  
-}
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 
 const Feed = () => {
@@ -21,7 +31,7 @@ const handleSearchChange = (e) => {
       <form className='relative w-full flex-center'>
         <input
           type='text'
-          placeholder='Search for a tag or a username'
+          placeholder = 'Search for a tag or a username'
           value={searchText}
           onChange={handleSearchChange}
           required
