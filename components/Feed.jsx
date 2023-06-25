6f67bdd46b950b3c542f8ff21cab4fa985ed3952
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-25 17:47:04 +0300

    new feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -22,9 +22,23 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
 
 const [searchText, setSearchText] = useState(' ');
+const [posts, setPosts] = useState([])
 const handleSearchChange = (e) => {
 
 }
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt')
+    const data = await response.json();
+
+    setPosts(data)
+  }
+
+  fetchPosts();
+}, [])
+
+
 
   return (
     <section className='feed'>
